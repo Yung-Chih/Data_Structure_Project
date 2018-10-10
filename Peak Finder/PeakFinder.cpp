cb46fc23284f42a0afa596c862df48fab5ba33b3
ricky <cc2297.fn@gmail.com>
2018-10-10 11:08:03 +0800

    final version

--- Peak Finder/PeakFinder.cpp
+++ Peak Finder/PeakFinder.cpp
@@ -1,50 +1,57 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
-short row,col;
-long int test[3][1000];
-pair<long int, long int> ans[1000*1000];
-int cnt;
+short m,n;
+vector< long int > test[3];
+vector< pair<long int,long int> > point;
 
 inline bool peak(short x, short y);
 
 int main(int argc, char const *argv[])
 {
+
     string root = argv[1];
+    string out = argv[1];
+    root += "\\matrix.data";
+    out += "\\final.peak";
     
-    ifstream fin( (root + "\\matrix.data").c_str() , ios::in);
-    ofstream fout( (root + "\\final.peak").c_str(), ios::out);
+    ifstream fin( root.c_str() , ios::in);
+    ofstream fout( out.c_str(), ios::out);
 
+    fin >> m >> n;
+    test[0].resize(n);
+    test[1].resize(n);
+    test[2].resize(n);
 
-    fin >> row >> col;
     short i,j;
-    for(j=0; j<col; j++)
+    for( j=0; j<n; j++){
         fin >> test[0][j];
+    }
 
-    for(i=1; i<row;i++){
-        for(j=0;j<col;j++){
+    for( i=1; i<m; i++){
+        for( j=0; j<n; j++){
             fin >> test[i%3][j];
             if( peak(i-1,j) ){
-                ans[cnt].first = i;
-                ans[cnt].second = j+1;
-                cnt++;
+                point.push_back( pair<long int, long int>(i,j+1) );
             }
         }
     }
 
-    for(j=0; j<col; j++){
-        if( peak(row-1,j) ){
-            ans[cnt].first = row;
-            ans[cnt].second = j+1;
-            cnt++;
+    for( j=0; j<n; j++){
+        if( peak(m-1,j) ){
+            point.push_back( pair<long int, long int>(m,j+1) );
         }
     }
 
-    fout << cnt << '\n';
-    for(int i=0; i<cnt; i++)
-        fout << ans[i].first << ' ' << ans[i].second << '\n';
+    fout << point.size() << '\n';
+
+    for( int i=0; i<point.size(); i++){
+        fout << point[i].first << ' ' << point[i].second << '\n';
+    }
 
     return 0;
 }
@@ -52,8 +59,8 @@ int main(int argc, char const *argv[])
 inline bool peak(short x, short y){
     bool valid = true;
     if( x > 0 )     valid &= test[x%3][y] >= test[(x+2)%3][y];
-    if( x < row-1 )   valid &= test[x%3][y] >= test[(x+1)%3][y];
+    if( x < m-1 )   valid &= test[x%3][y] >= test[(x+1)%3][y];
     if( y > 0 )     valid &= test[x%3][y] >= test[x%3][y-1];
-    if( y < col-1 )   valid &= test[x%3][y] >= test[x%3][y+1];
+    if( y < n-1 )   valid &= test[x%3][y] >= test[x%3][y+1];
     return valid;
 }
